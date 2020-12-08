54d93ae32ac9d0397eb62cf820e7413c10ee1d06
DeanC <dean.clancy91@gmail.com>
2020-12-07 19:10:31 -0800

    Add Switch and Route components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 // Components
 import AboutUs from "./pages/AboutUs";
 import Nav from "./components/Nav";
+import Contact from "./pages/Contact";
+import OurWork from "./pages/OurWork";
 // Styles
 import GlobalStyles from "./styles/GlobalStyles";
+// React Router
+import { Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <GlobalStyles />
       <Nav />
-      <AboutUs />
+      <Switch>
+        <Route path="/" exact>
+          <AboutUs />
+        </Route>
+        <Route path="/work">
+          <OurWork />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+      </Switch>
     </div>
   );
 }
